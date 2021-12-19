01e9d966c34c7919f7073e59ad75143fb358b400
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-12-19 13:17:06 +0500

    .....

---
+++ js-udemy-recall.js
@@ -0,0 +1,19 @@
+// const day = "tuesday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("dushanba");
+//     break;
+//   case "tuesday":
+//     console.log("seshanba");
+//     break;
+//   default:
+//     console.log("unday kun yo`q");
+// }
+
+const age = 19;
+
+// const availability = age >= 18 ? "can drive a car" : "can`t drive a car";
+// console.log(availability);
+
+// console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
