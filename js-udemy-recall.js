2f0232fa9a561c57e4d916e04ce20343367e7f2a
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-12-20 13:23:19 +0500

    .....

--- js-udemy-recall.js
+++ js-udemy-recall.js
@@ -1,3 +1,5 @@
+// ---------------------------- switch statements -----------------------------------
+
 // const day = "tuesday";
 
 // switch (day) {
@@ -11,9 +13,34 @@
 //     console.log("unday kun yo`q");
 // }
 
-const age = 19;
+// ------------------ Conditional(Turnary) Operator ----------------------------------
+
+// const age = 19;
 
 // const availability = age >= 18 ? "can drive a car" : "can`t drive a car";
 // console.log(availability);
 
 // console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+
+// const bill = 430;
+
+// const tip = 50 <= bill && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill} , the tip was ${tip}, and the total value ${bill + tip}`
+// );
+
+// ------------------- The same as below -------------------------------------------------
+
+// if (bill >= 50 && bill <= 300) {
+//   console.log(bill + bill * tip);
+// } else {
+//   console.log(bill + bill * 0.2);
+// }
+
+// console.log("23" + "10" - 3);
+"use strict";
+let hasLicense = false;
+let passedTest = true;
+
+if (passedTest) haslicense = true;
+if (hasLicense) console.log("can drive");
